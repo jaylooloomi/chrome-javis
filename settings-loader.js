48805a04fec8205928b5cdbabf51f4277d306218
Chrome Jarvis Dev <dev@chrome-jarvis.local>
2026-02-27 22:04:28 +0800

    fix: 解決 CSP 內聯 script 違規 - 使用外部 loader 文件加載 i18n

---
+++ settings-loader.js
@@ -0,0 +1,7 @@
+// settings-loader.js - 在 settings.js 之前加載 i18n
+import i18n from './i18n/i18n.js';
+
+// 將 i18n 暴露到全局作用域，供 settings.js 使用
+window.i18n = i18n;
+
+console.log('[Settings Loader] i18n 已導入並暴露到全局');
